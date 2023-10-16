61a6063f47b53bc876a90587713cb75ab19fcaad
AlekseyKashubin <Akashubin@yahoo.com>
2023-10-16 13:23:59 -0700

    updated-recipes

--- flask_app/controllers/table_controller.py
+++ flask_app/controllers/table_controller.py
@@ -12,6 +12,7 @@ def Home():
 def Brew():
     return render_template('brew.html')
 
+# Region routes
 @app.route('/kalita')
 def region_kalita():
     return render_template('region_kalita.html')
@@ -26,7 +27,7 @@ def region_chemex():
 
 @app.route('/aeropress')
 def region_aeropress():
-    return render_template('region_aeropress.html')
+    return render_template('region_aero.html')
 
 @app.route('/frenchpress')
 def region_frenchpress():
@@ -38,12 +39,15 @@ def region_espresso():
 
 @app.route('/mokapot')
 def region_mokapot():
-    return render_template('region_mokapot.html')
+    return render_template('region_moka.html')
 
 @app.route('/autodrip')
 def region_autodrip():
     return render_template('region_autodrip.html')
 
+
+
+# Kalita Recipies 
 @app.route('/kalita_africa')
 def kalita_africa_page():
     return render_template('kalita_africa.html')
@@ -67,6 +71,196 @@ def kalita_central_america_page():
 @app.route('/kalita_hawaiian')
 def kalita_hawaiian_page():
     return render_template('kalita_hawaiian.html')
+
+
+
+
+# V60 Recipes
+@app.route('/v60_africa')
+def v60_africa_page():
+    return render_template('v60_africa.html')
+
+@app.route('/v60_south_america')
+def v60_south_america_page():
+    return render_template('v60_south_america.html')
+
+@app.route('/v60_caribbean')
+def v60_caribbean_page():
+    return render_template('v60_caribbean.html')
+
+@app.route('/v60_asia')
+def v60_asia_page():
+    return render_template('v60_asia.html')
+
+@app.route('/v60_central_america')
+def v60_central_america_page():
+    return render_template('v60_central_america.html')
+
+@app.route('/v60_hawaiian')
+def v60_hawaiian_page():
+    return render_template('v60_hawaiian.html')
+
+
+
+
+# Areopress Recipes
+@app.route('/aero_africa')
+def aero_africa_page():
+    return render_template('aero_africa.html')
+
+@app.route('/aero_south_america')
+def aero_south_america_page():
+    return render_template('aero_south_america.html')
+
+@app.route('/aero_caribbean')
+def aero_caribbean_page():
+    return render_template('aero_caribbean.html')
+
+@app.route('/aero_asia')
+def aero_asia_page():
+    return render_template('aero_asia.html')
+
+@app.route('/aero_central_america')
+def aero_central_america_page():
+    return render_template('aero_central_america.html')
+
+@app.route('/aero_hawaiian')
+def aero_hawaiian_page():
+    return render_template('aero_hawaiian.html')
+
+
+# Chemex Recipes
+@app.route('/chemex_africa')
+def chemex_africa_page():
+    return render_template('chemex_africa.html')
+
+@app.route('/chemex_south_america')
+def chemex_south_america_page():
+    return render_template('chemex_south_america.html')
+
+@app.route('/chemex_caribbean')
+def chemex_caribbean_page():
+    return render_template('chemex_caribbean.html')
+
+@app.route('/chemex_asia')
+def chemex_asia_page():
+    return render_template('chemex_asia.html')
+
+@app.route('/chemex_central_america')
+def chemex_central_america_page():
+    return render_template('chemex_central_america.html')
+
+@app.route('/chemex_hawaiian')
+def chemex_hawaiian_page():
+    return render_template('chemex_hawaiian.html')
+
+
+
+# Espresso Recipes
+@app.route('/espresso_africa')
+def espresso_africa_page():
+    return render_template('espresso_africa.html')
+
+@app.route('/espresso_south_america')
+def espresso_south_america_page():
+    return render_template('espresso_south_america.html')
+
+@app.route('/espresso_caribbean')
+def espresso_caribbean_page():
+    return render_template('espresso_caribbean.html')
+
+@app.route('/espresso_asia')
+def espresso_asia_page():
+    return render_template('espresso_asia.html')
+
+@app.route('/espresso_central_america')
+def espresso_central_america_page():
+    return render_template('espresso_central_america.html')
+
+@app.route('/espresso_hawaiian')
+def espresso_hawaiian_page():
+    return render_template('espresso_hawaiian.html')
+
+
+# French Press Recipes
+@app.route('/frenchpress_africa')
+def frenchpress_africa_page():
+    return render_template('frenchpress_africa.html')
+
+@app.route('/frenchpress_south_america')
+def frenchpress_south_america_page():
+    return render_template('frenchpress_south_america.html')
+
+@app.route('/frenchpress_caribbean')
+def frenchpress_caribbean_page():
+    return render_template('frenchpress_caribbean.html')
+
+@app.route('/frenchpress_asia')
+def frenchpress_asia_page():
+    return render_template('frenchpress_asia.html')
+
+@app.route('/frenchpress_central_america')
+def frenchpress_central_america_page():
+    return render_template('frenchpress_central_america.html')
+
+@app.route('/frenchpress_hawaiian')
+def frenchpress_hawaiian_page():
+    return render_template('frenchpress_hawaiian.html')
+
+
+
+# MokaPot Recipes
+@app.route('/moka_africa')
+def moka_africa_page():
+    return render_template('moka_africa.html')
+
+@app.route('/moka_south_america')
+def moka_south_america_page():
+    return render_template('moka_south_america.html')
+
+@app.route('/moka_caribbean')
+def moka_caribbean_page():
+    return render_template('moka_caribbean.html')
+
+@app.route('/moka_asia')
+def moka_asia_page():
+    return render_template('moka_asia.html')
+
+@app.route('/moka_central_america')
+def moka_central_america_page():
+    return render_template('moka_central_america.html')
+
+@app.route('/moka_hawaiian')
+def moka_hawaiian_page():
+    return render_template('moka_hawaiian.html')
+
+
+# Auto Drip Recipes
+@app.route('/autodrip_africa')
+def autodrip_africa_page():
+    return render_template('autodrip_africa.html')
+
+@app.route('/autodrip_south_america')
+def autodrip_south_america_page():
+    return render_template('autodrip_south_america.html')
+
+@app.route('/autodrip_caribbean')
+def autodrip_caribbean_page():
+    return render_template('autodrip_caribbean.html')
+
+@app.route('/autodrip_asia')
+def autodrip_asia_page():
+    return render_template('autodrip_asia.html')
+
+@app.route('/autodrip_central_america')
+def autodrip_central_america_page():
+    return render_template('autodrip_central_america.html')
+
+@app.route('/autodrip_hawaiian')
+def autodrip_hawaiian_page():
+    return render_template('autodrip_hawaiian.html')
+
+
 
 
 @app.route('/grounds')
